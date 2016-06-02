755e177344dd3a636bbea7b4310f9d087db99852
Gloria Marín <gloria.marinortiz@alumnos.upm.es>
2016-06-02 18:37:24 +0200

    Tema 8:Base de Datos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,28 @@
-var express = require('express');
-var app = express();
+var models = require('../models');
 
 exports.question = function (req, res, next){
-	var answer= req.query.answer ||"";
-	res.render('quizzes/question', {question: 'Capital de Italia', answer: answer});
+	models.Quiz.findOne().then(function(quiz){ 
+	if (quiz) {
+		var answer = req.query.answer || '';
+		res.render('quizzes/question', {question: quiz.question,
+								                answer: answer});
+		    } else {
+		    	throw new Error('No hay preguntas en la BBDD.');
+		    }
+		}).catch(function(error) {
+			next(error);});
+	
 };
- exports.check = function(req, res, next){
- 	var answer= req.query.answer ||"";
- 	var result = req.query.answer === 'Roma' ? 'Correcta' : 'Incorrecta';
- 	res.render('quizzes/result',{result:result, answer:answer});
 
- };
+ exports.check = function(req, res, next){
+ 	models.Quiz.findOne() // Busca la primera pregunta existente
+		.then(function(quiz) {
+			if (quiz) {
+				var answer = req.query.answer || "";
+				res.render('quizzes/result', { result: result, 
+											   answer: answer });
+		    } else {
+		    	throw new Error('No hay preguntas en la BBDD.');
+		    }
+		}).catch(function(error) {	next(error);});
+};
