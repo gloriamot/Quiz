42a2ef4b970a3ae1bdfa454ae8ccabed16d45d8a
Gloria Marín <gloria.marinortiz@alumnos.upm.es>
2016-06-02 13:16:44 +0200

    Tema 4: No perder respuesta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 
 exports.question = function (req, res, next){
-	res.render('quizzes/question', {question: 'Capital de Italia'});
+	var answer= req.query.answer ||"";
+	res.render('quizzes/question', {question: 'Capital de Italia', answer: answer});
 };
  exports.check = function(req, res, next){
+ 	var answer= req.query.answer ||"";
  	var result = req.query.answer === 'Roma' ? 'Correcta' : 'Incorrecta';
- 	res.render('quizzes/result',{result:result});
+ 	res.render('quizzes/result',{result:result, answer:answer});
 
  };
